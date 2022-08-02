0518e281afb10122df64024ff17124f4ca7efbfe
Tomasz Kluza <tomasz.kluza@picard.de>
2022-08-03 00:00:23 +0300

    feature: Step 1 - before tests implementation for Place domain

---
+++ src/test/kotlin/com/tkluza/spring/efficientunittests/business/place/domain/PlaceFacadeTest.kt
@@ -0,0 +1,15 @@
+package com.tkluza.spring.efficientunittests.business.place.domain
+
+import com.tkluza.spring.efficientunittests.common.BaseTest
+import org.springframework.beans.factory.annotation.Autowired
+
+class PlaceFacadeTest : BaseTest() {
+
+    @Autowired
+    private lateinit var placeFacade: PlaceFacade
+
+    // TODO("Nested PlaceTests with required tests")
+    // TODO("Test case: find by name")
+    // TODO("Test case: find by another name. Entity will be added in test")
+    // TODO("Test case: EntityNotFoundException")
+}
