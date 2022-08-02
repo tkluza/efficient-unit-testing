38dfacbc58a61255e7f959cc0679c419ce3fba06
Tomasz Kluza <tomasz.kluza@picard.de>
2022-08-03 00:18:21 +0300

    feature: Javadoc

--- src/main/kotlin/com/tkluza/spring/efficientunittests/business/place/domain/PlaceFacade.kt
+++ src/main/kotlin/com/tkluza/spring/efficientunittests/business/place/domain/PlaceFacade.kt
@@ -3,9 +3,18 @@ package com.tkluza.spring.efficientunittests.business.place.domain
 import com.tkluza.spring.efficientunittests.business.place.dto.query.PlaceQuery
 import com.tkluza.spring.efficientunittests.business.place.dto.query.SeatQuery
 
+/**
+ * Main facade for Place domain
+ */
 interface PlaceFacade {
 
+    /**
+     * Finds [PlaceQuery] by its name
+     */
     fun findPlaceByName(name: String): PlaceQuery
 
+    /**
+     * Finds [SeatQuery] by its id
+     */
     fun findSeatById(id: Long): SeatQuery
 }
--- src/main/kotlin/com/tkluza/spring/efficientunittests/business/user/domain/UserFacade.kt
+++ src/main/kotlin/com/tkluza/spring/efficientunittests/business/user/domain/UserFacade.kt
@@ -2,7 +2,13 @@ package com.tkluza.spring.efficientunittests.business.user.domain
 
 import com.tkluza.spring.efficientunittests.business.user.dto.query.UserQuery
 
+/**
+ * Main facade for User domain
+ */
 interface UserFacade {
 
+    /**
+     * Finds [UserQuery] by its id
+     */
     fun findUserById(id: Long): UserQuery
 }
